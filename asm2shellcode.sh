34698f531b71d9af5c408ed3420859d8ea0bfb5c
Taolaw <magicgoogol@gmail.com>
2020-12-06 16:33:50 +0800

    fix bug

--- asm2shellcode.sh
+++ asm2shellcode.sh
@@ -7,6 +7,7 @@ as -o $1.o $1
 echo '[+] Linking ...'
 ld -s -o $1.bin $1.o
 echo ''
+echo ''
 echo '[+] Generating shellcode ...'
 for i in $(objdump -d ./$1.bin |grep "^ " |cut -f2); do echo -n '\x'$i >> sc.txt; done; echo
 cat sc.txt
